1f5856bd57a8f3d1291ab6508e92a05d999d851a
shashankjain <sjshashank313@example.com>
2020-09-07 23:59:28 -0700

    ALL done

---
+++ login/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render, HttpResponse
+
+# Create your views here.
+
+def index(request):
+    return render(request, 'index.html')
+
+def home(request):
+    return  HttpResponse(" You are Logged in ")
